229dcad0a099e09763cd20e8757ec8234fc9effe
Talvane A. de Magalhães <talvane.magalhaes@gmail.com>
2021-08-07 17:42:22 -0300

    add jwt authentication

--- desafio/urls_api.py
+++ desafio/urls_api.py
@@ -2,6 +2,9 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView, TokenVerifyView
+    )
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -18,6 +21,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('core/', include('desafio.core.urls', namespace='core')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
