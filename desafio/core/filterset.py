3ff00d158ffb2195e6dbdb2ed44325695465bddf
Talvane A. de Magalhães <talvane.magalhaes@gmail.com>
2021-08-07 17:32:15 -0300

    add filterset in City

--- desafio/core/filterset.py
+++ desafio/core/filterset.py
@@ -1,6 +1,6 @@
 from desafio.filterset import SearchFilterSet
 
-from .models import State
+from .models import State, City
 
 
 class StateFilter(SearchFilterSet):
@@ -9,3 +9,15 @@ class StateFilter(SearchFilterSet):
         model = State
         fields = ['q']
         search_fields = ['code__icontains', 'name__icontains']
+
+
+class CityFilter(SearchFilterSet):
+    class Meta:
+        model = City
+        fields = ['q']
+        search_fields = [
+            'name__icontains',
+            'slug__icontains',
+            'state__code__icontains',
+            'state__name__icontains'
+        ]
--- desafio/core/views.py
+++ desafio/core/views.py
@@ -3,7 +3,7 @@ from django_filters import rest_framework as filters
 
 from .models import State, City
 from .serializers import CitySerializer, StateSerializer
-from desafio.core.filterset import StateFilter
+from desafio.core.filterset import CityFilter, StateFilter
 
 
 class StateViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
@@ -17,4 +17,4 @@ class CityViewSet(viewsets.ModelViewSet):
     queryset = City.objects.all()
     serializer_class = CitySerializer
     filter_backends = (filters.DjangoFilterBackend,)
-    # filter_class = ''
+    filter_class = CityFilter
