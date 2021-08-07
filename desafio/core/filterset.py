d9e97eab9393602c52f018d96f1492985e1bca54
Talvane A. de Magalhães <talvane.magalhaes@gmail.com>
2021-08-07 17:17:26 -0300

    add FilterSet in StateViewSet

---
+++ desafio/core/filterset.py
@@ -0,0 +1,12 @@
+from django.db import models
+from desafio.filterset import SearchFilterSet
+
+from .models import State
+
+
+class StateFilter(SearchFilterSet):
+
+    class Meta:
+        model = State
+        fields = ['q']
+        search_fields = ['code__icontains', 'name__icontains']
--- desafio/core/views.py
+++ desafio/core/views.py
@@ -3,10 +3,11 @@ from django_filters import rest_framework as filters
 
 from .models import State
 from .serializers import StateSerializer
+from desafio.core.filterset import StateFilter
 
 
 class StateViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     queryset = State.objects.all()
     serializer_class = StateSerializer
     filter_backends = (filters.DjangoFilterBackend,)
-    # filter_class = ''
+    filter_class = StateFilter
