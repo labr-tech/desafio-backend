d457fa2aa633a4c88fa4f52064dcd6581beab858
Talvane A. de Magalhães <talvane.magalhaes@gmail.com>
2021-08-07 16:26:04 -0300

    add model and admin

--- desafio/core/admin.py
+++ desafio/core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import State, City
+
+
+class CityInLine(admin.TabularInline):
+    model = City
+    extra = 1
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class StateModelAdmin(admin.ModelAdmin):
+    inlines = (CityInLine,)
+    list_display = ('code', 'name')
+    search_fields = list_display
+    list_filter = list_display
+
+
+admin.site.register(State, StateModelAdmin)
--- desafio/core/models.py
+++ desafio/core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from desafio.utils import CommonModel
+
+
+class State(CommonModel):
+    code = models.CharField(verbose_name=_('Sigla do Estado'), max_length=2)
+    name = models.CharField(verbose_name=_('Nome do Estado'), max_length=255)
+
+    class Meta:
+        verbose_name = _('Estado')
+        verbose_name_plural = _('Estados')
+        ordering = ('code',)
+
+    def __str__(self) -> str:
+        return self.code
+
+
+class City(CommonModel):
+    state = models.ForeignKey(
+        'State', on_delete=models.CASCADE, verbose_name=_('Cidade')
+    )
+    name = models.CharField(verbose_name=_('Nome da Cidade'), max_length=255)
+    slug = models.SlugField(verbose_name=_('Slug Cidade'), default='')
+
+    class Meta:
+        verbose_name = _('Cidade')
+        verbose_name_plural = _('Cidades')
+        ordering = ('name',)
+
+    def __str__(self) -> str:
+        return self.name
