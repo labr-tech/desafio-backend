c90320019bc010435d8e655b3962a85ba3a75258
Talvane A. de Magalhães <talvane.magalhaes@gmail.com>
2021-08-08 11:19:40 -0300

    fix serializer

--- desafio/core/serializers.py
+++ desafio/core/serializers.py
@@ -1,14 +1,19 @@
 from rest_framework import serializers
 from django.db.transaction import atomic
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.utils.translation import gettext_lazy as _
 from django.utils.text import slugify
+from rest_framework.validators import UniqueValidator
 
 from .models import State, City
 
 
 class StateSerializer(serializers.ModelSerializer):
     name = serializers.ReadOnlyField()
+    code = serializers.CharField(
+        max_length=2,
+        validators=[UniqueValidator(queryset=State.objects.all())]
+    )
 
     class Meta:
         model = State
@@ -37,7 +42,15 @@ class CitySerializer(serializers.ModelSerializer):
         copy_validated_data['state'] = state
         copy_validated_data['slug'] = slugify(validated_data['name'])
 
-        return super().create(copy_validated_data)
+        try:
+            return super().create(copy_validated_data)
+        except ValidationError:
+            raise serializers.ValidationError(
+                _(
+                    'Registro já existente. Estado: ' + self.data['state'] +
+                    ', Cidade: ' + self.data['name']
+                )
+            )
 
     @atomic
     def update(self, instance, validated_data):
